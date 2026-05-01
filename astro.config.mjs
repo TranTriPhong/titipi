8f85f2159c40b5af85b28dbf7554fd47996754fd
Phong Tri Tran <tr4ntriphong@gmail.com>
2026-05-01 15:19:26 +0700

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite"; // Nhập plugin mới
+import tailwindcss from "@tailwindcss/vite";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()], // Nạp Tailwind qua Vite
+    plugins: [tailwindcss()],
   },
   markdown: {
+    // Note to self: Phải nạp đúng thứ tự: Math trước, Katex sau
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
