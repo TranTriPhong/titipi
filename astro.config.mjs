f1700580d1ca3dbaadda13561a34c39ad7e76d69
Phong Tri Tran <tr4ntriphong@gmail.com>
2026-05-02 22:08:09 +0700

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
-
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  // 1. Thêm dòng này để kích hoạt lai giữa tĩnh (tốc độ cao) và động (gọi AI)
-  output: 'hybrid',
-
+  output: 'static',
   vite: {
     plugins: [tailwindcss()],
   },
-
   markdown: {
-    // Note to self: Phải nạp đúng thứ tự: Math trước, Katex sau
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
-
   integrations: [sitemap()],
   adapter: vercel(),
 });
